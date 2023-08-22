ea883ec310f4fd52201303be3d2ce2f05f848b0d
Asadu <techasadbd@gmail.com>
2023-08-22 12:17:46 +0600

    Initial commit

---
+++ app/src/main/java/com/example/dialogbox/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.dialogbox;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button;
+    private AlertDialog.Builder alertDialog;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.exitVButtonId);
+
+        button.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        
+        // Alert Dialog Create
+        alertDialog = new AlertDialog.Builder(MainActivity.this);
+        
+        // Alert Dialog Set Title
+        alertDialog.setTitle("Alert");
+        
+        // Alert Dialog set Message
+        alertDialog.setMessage("Are you want to exit");
+
+        // Alert Dialog set Message
+        alertDialog.setIcon(R.drawable.icon);
+
+        // Alert Dialog set Positive Button
+        alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // Exit or Closed
+                finish();
+            }
+        });
+
+        // Alert Dialog set Negative Button
+        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "Negative Button is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        // Alert Dialog set Neutral Button
+        alertDialog.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "Cancel Button is clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        
+        alertDialog.show();
+    }
+}
